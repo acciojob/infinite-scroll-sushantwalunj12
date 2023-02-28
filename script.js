8af2d4dac058f6e88099e0a081476badef5eed68
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-02-28 20:02:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+const infiList = document.getElementById('infi-list');
+let counter = 10;
 
+// Add 10 list items to the list by default
+for (let i = 1; i <= counter; i++) {
+  const listItem = document.createElement('li');
+  listItem.innerText = `List item ${i}`;
+  infiList.appendChild(listItem);
+}
+
+// Add 2 more list items when the user reaches the end of the list
+infiList.addEventListener('scroll', () => {
+  if (infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight) {
+    for (let i = counter + 1; i <= counter + 2; i++) {
+      const listItem = document.createElement('li');
+      listItem.innerText = `List item ${i}`;
+      infiList.appendChild(listItem);
+    }
+    counter += 2;
+  }
+});
